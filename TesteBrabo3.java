a88452dd861c1ec15f184658ebb2d0ed1c7337c9
SnowzinHue <68404294+SnowzinHue@users.noreply.github.com>
2022-10-18 11:08:59 -0300

    Add files via upload

---
+++ TesteBrabo3.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package teste.brabo;
+
+/**
+ *
+ * @author Danilo Torquato de Oliveira 2Ds
+ */
+public class TesteBrabo {
+  
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+       System.out.println((3+ (7+3)) * 2);
+    }
+    
+}
